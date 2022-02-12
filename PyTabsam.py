534b0d7b9786b1c26bd420ebf5c68ac137c92127
Grüebler Michael (SSZ) <sszgrm@global.szh.loc>
2022-02-12 23:18:18 +0100

    Read footnotes an add to dataframe

--- PyTabsam.py
+++ PyTabsam.py
@@ -7,7 +7,7 @@ import pandas as pd
 import os # operating system
 import re # regular expressions
 from openpyxl import load_workbook
-# @Hansjˆrg: Diese Seite kˆnnte interessant sein um Inhalte zu kopieren:
+# @Hansj√∂rg: Diese Seite k√∂nnte interessant sein um Inhalte zu kopieren:
 # https://stackoverflow.com/questions/42344041/how-to-copy-worksheet-from-one-workbook-to-another-one-using-openpyxl
 
 # Leere Listen vorbereiten
@@ -16,6 +16,8 @@ count_sheet = 0
 data_sheet = pd.DataFrame([],dtype=pd.StringDtype())
 count_expl = 0
 data_expl  = pd.DataFrame([],dtype=pd.StringDtype())
+count_foot = 0
+data_foot  = pd.DataFrame([],dtype=pd.StringDtype())
 
 # Function tolog
 # Write logging information
@@ -49,7 +51,7 @@ def create_sampledata():
   list_coll = []
   elem_list_coll = [1, "Meteorologie", "O:/Projekte/PyTabsam/Testfaelle-Input/02_02", "Meteorologie.xlsx"]
   list_coll.append(elem_list_coll)
-  elem_list_coll = [2, "Luftqualit‰t", "O:/Projekte/PyTabsam/Testfaelle-Input/07_03", "Luftqualitaet.xlsx"]
+  elem_list_coll = [2, "Luftqualit√§t", "O:/Projekte/PyTabsam/Testfaelle-Input/07_03", "Luftqualitaet.xlsx"]
   list_coll.append(elem_list_coll)
   elem_list_coll = [3, "Abfallentsorgung", "O:/Projekte/PyTabsam/Testfaelle-Input/07_02", "Abfallentsorgung.xlsx"]
   list_coll.append(elem_list_coll)
@@ -57,14 +59,14 @@ def create_sampledata():
   
   # SHEET
   list_sheet = []
-  elem_list_sheet = [1, 1, "T_02.02.01.2017.xlsm", "O:/Projekte/PyTabsam/Testfaelle-Input/02_02", "T02.02.01.2017", "T_2.2.1.2017", "Wetterrekorde", "Station Z¸rich Fluntern", "historisch und 2017", "MeteoSchweiz", 1]
+  elem_list_sheet = [1, 1, "T_02.02.01.2017.xlsm", "O:/Projekte/PyTabsam/Testfaelle-Input/02_02", "T02.02.01.2017", "T_2.2.1.2017", "Wetterrekorde", "Station Z√ºrich Fluntern", "historisch und 2017", "MeteoSchweiz", 1]
   list_sheet.append(elem_list_sheet)
   data_sheet = pd.DataFrame(list_sheet, columns = ['ID', 'FK_collection', 'filename', 'directory', 'sheet_name', 'code', 'title', 'subtitle1', 'subtitle2', 'source', 'order'])
   count_sheet = 1
   
   # EXPLANATION
   list_expl = []
-  elem_list_expl = [1, 1, "T_02.02.0.Erl‰uterung.xlsm", "O:/Projekte/PyTabsam/Testfaelle-Input/02_02"]
+  elem_list_expl = [1, 1, "T_02.02.0.Erl√§uterung.xlsm", "O:/Projekte/PyTabsam/Testfaelle-Input/02_02"]
   list_expl.append(elem_list_expl)
   data_expl = pd.DataFrame(list_expl, columns = ['ID', 'FK_collection', 'filename', 'directory'])
   count_expl = 1
@@ -84,9 +86,9 @@ def read_coll_dir():
     # get a list of all the files in the directory
     files = [file for file in os.listdir(collection_path)]
     for filename in files:
-      is_expl = re.match(r'T_\d+.\d+.\d+.Erl‰uterungen.xlsm', filename)
+      is_expl = re.match(r'T_\d+.\d+.\d+.Erl√§uterungen.xlsm', filename)
       # Regular Expressen for an excel file containing an explanation
-      # Example: T_02.02.0.Erl‰uterungen.xlsm
+      # Example: T_02.02.0.Erl√§uterungen.xlsm
       is_sheet = re.match(r'(T_|T_G)\d+.\d+.\d+(.xlsm|.\d+.xlsm|\w.xlsm)', filename)
       # Regular Expressen for an excel file containing data
       # Examples: T_02.02.03.xlsm, T_02.02.01.2017.xlsm, T_G03.03.01.xlsm, T_G07.03.05a
@@ -111,7 +113,7 @@ def read_coll_dir():
 # Read excel that contains the explanation
 def read_xls_expl():
   # Test to read single cells from one sheet
-  wb = load_workbook(filename = r'O:\Projekte\PyTabsam\Testfaelle-Input\07_03\T_07.03.0.Erl‰uterungen.xlsm', read_only=True)
+  wb = load_workbook(filename = r'O:\Projekte\PyTabsam\Testfaelle-Input\07_03\T_07.03.0.Erl√§uterungen.xlsm', read_only=True)
   sheet_ranges = wb['Internet']
   print(sheet_ranges['A1'].value)
   print(sheet_ranges['B1'].value)
@@ -119,7 +121,7 @@ def read_xls_expl():
   print(sheet_ranges['B2'].value)
 
 # Function read_xls_metadata
-# Read metadata out of excel 
+# Read metadata of excel 
 def read_xls_metadata(sheet_id):
   global data_sheet
   xls_filename = data_sheet.loc[data_sheet['ID'] == sheet_id, 'filename'].values[0]
@@ -151,16 +153,46 @@ def read_xls_metadata(sheet_id):
         #print("Quelle: " + str(source_cell._value))
         data_sheet.loc[data_sheet['ID'] == sheet_id, ['source']] = str(source_cell._value)
 
-# Function read_all_metadata
-# Read metadata of all sheets in dataframe 
-def read_all_metadata():
-  global count_sheet, data_sheet
+# Function read_xls_footnote
+# Read footnote of excel 
+def read_xls_footnote(sheet_id, list_foot):
+  global data_sheet, count_foot, data_foot
+  xls_filename = data_sheet.loc[data_sheet['ID'] == sheet_id, 'filename'].values[0]
+  xls_directory = data_sheet.loc[data_sheet['ID'] == sheet_id, 'directory'].values[0]
+  xls_path = xls_directory + "/" + xls_filename
+  #print("Excel: "+ xls_path)
+  wb = load_workbook(filename = xls_path)
+  sheet_fn = wb['Fussnoten']
+  coltype = ""
+  colfnid = ""
+  for (row, col), source_cell in sheet_fn._cells.items():
+    #print(str(row) + " - "+ str(col) + ": " + str(source_cell._value))
+    if col==1:
+      coltype = str(source_cell._value).lower()
+    if col==2:
+      colfnid = str(source_cell._value).lower()
+    if col==3:
+      if coltype=="<web>":
+        count_foot += 1 
+        #print("Fussnote Nr: " + colfnid + " = " + str(source_cell._value))
+        elem_list_foot = [count_foot, sheet_id, colfnid, str(source_cell._value)]
+        list_foot.append(elem_list_foot)
+
+
+# Function read_all_md_fn
+# Read metadata and footnote sheets of all excel documents in sheet dataframe 
+# As it is very inefficient to grow a dataframe, the footnotes are collectet
+# in a list, which is then converted to a dataframe
+def read_all_md_fn():
+  global count_sheet, data_sheet, data_foot
   #print(count_sheet)
   sheet_id = 0
+  list_foot = []
   while sheet_id < count_sheet:
     sheet_id += 1
     read_xls_metadata(sheet_id)
-
+    read_xls_footnote(sheet_id, list_foot)
+    data_foot = pd.DataFrame(list_foot, columns = ['ID', 'FK_sheet', 'code', 'text'])
 
 # Main progam
 def main():
@@ -175,8 +207,8 @@ def main():
   create_sampledata()
   
   #read_xls_expl()
-  print("Open all excel sheets, read metadata and add to dataframe")
-  read_all_metadata()
+  print("Open all excel sheets, read metadata and footnotes and add to dataframes")
+  read_all_md_fn()
   
   #print(data_coll)
   #print(data_sheet)
