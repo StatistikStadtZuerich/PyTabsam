b419fd1024af8796591e93ab5c78a9d776cc36d6
hjstalder <hansi.stalder@gmail.com>
2022-02-22 14:21:55 +0100

    Review_2022_02_22

--- PyTabsam.py
+++ PyTabsam.py
@@ -55,7 +55,7 @@ def create_sampledata():
   list_coll.append(elem_list_coll)
   elem_list_coll = [3, "Abfallentsorgung", "O:/Projekte/PyTabsam/Testfaelle-Input/07_02", "Abfallentsorgung.xlsx"]
   list_coll.append(elem_list_coll)
-  data_coll = pd.DataFrame(list_coll, columns = ['id', 'title' , 'input_path', 'output_path'])
+  data_coll = pd.DataFrame(list_coll, columns = ['id', 'title' , 'input_path', 'output_filename'])
   
   # SHEET
   list_sheet = []
@@ -195,6 +195,113 @@ def read_all_md_fn():
     read_xls_footnote(sheet_id, list_foot)
     data_foot = pd.DataFrame(list_foot, columns = ['ID', 'FK_sheet', 'code', 'text'])
 
+
+# Function creat_tabsam 
+# Create and generate the destination excel files
+# If the destination files already exists, they will be overwritten
+def create_tabsam():
+    coll_ID = 0
+    for index, row in data_coll.iterrows():
+      
+      # Copy the template into the existing xlsx files according to the collection
+      coll_ID = row['id']
+      #print(coll_ID)
+      filename = row['output_filename']
+      shutil.copy('VorlageTabsam.xlsx', filename)
+      
+      # Fill in the creation date into the worksheet 'Inhalt' 
+      file = openpyxl.load_workbook(filename)
+      sheet = file["Inhalt"]
+      today = datetime.date.today()
+      create_date = "Erstellt am: " + today.strftime("%d.%m.%Y")
+      sheet.cell(row=2, column=3).value = create_date
+      file.save(filename)
+      
+      # Create and fill in the worksheet 'Erläuterungen' if exists
+      create_worksheet_expl(coll_ID, filename)
+
+      # Create and fill all worksheet according to the dataframe sheet
+      create_worksheets(coll_ID, filename)
+  
+
+# Function create_worksheet_expl
+def create_worksheet_expl(coll_ID, dest_file):
+  for index, row in data_expl.iterrows():
+    if coll_ID == row['FK_collection']:
+      # opening the source xlsx
+      source_xlsx = row['directory'] + "/" + row['filename']
+      #print(source_xlsx)
+      source_wb = openpyxl.load_workbook(source_xlsx)
+      source_ws = source_wb["Internet"]
+      
+      # opening the destination xlsx and create the new worksheet
+      dest_wb = openpyxl.load_workbook(dest_file)
+      dest_ws = dest_wb.create_sheet("Erläuterungen")
+
+      # calculate total number of rows and 
+      # columns in source excel file
+      mr = source_ws.max_row
+      mc = source_ws.max_column
+      
+      # copying the cell values from source 
+      # excel file to destination xlsx file
+      for i in range (1, mr + 1):
+        for j in range (1, mc + 1):
+          # reading cell value from source xlsx
+          c = source_ws.cell(row = i, column = j)
+  
+          # writing the read value to destination xlsx file
+          dest_ws.cell(row = i, column = j).value = c.value
+  
+      # Write "Erläuterungen" in the table of contents
+      content = dest_wb["Inhalt"]
+      content.cell(row=10, column=1).value = "Erläuterungen"
+      content.cell(row=10, column=2).value = ""
+  
+      # saving the destination xlsx file
+      dest_wb.save(dest_file)
+      
+
+# Function create_worksheets
+def create_worksheets(coll_ID, dest_file):
+  
+  # set the counter for the table of content
+  wb = openpyxl.load_workbook(dest_file, read_only=True)
+  if 'Erläuterungen' in wb.sheetnames:
+    count_toc = 11
+  else:
+    count_toc = 10
+        
+  for index, row in data_sheet.iterrows():
+    if coll_ID == row['FK_collection']:
+
+      # opening the destination xlsx and create the new worksheet
+      dest_wb = openpyxl.load_workbook(dest_file)
+      # neu sheetname
+      dest_ws = dest_wb.create_sheet(row['sheet_name'])
+      
+      # Write the code, title, subtitle1, subtitle2, source
+      dest_ws.cell(row=1, column=1).value = row['code']
+      dest_ws.cell(row=2, column=1).value = row['title']
+      dest_ws.cell(row=3, column=1).value = row['subtitle1']
+      if(row['subtitle2'] != "None"):
+        dest_ws.cell(row=4, column=1).value = row['subtitle2']
+        dest_ws.cell(row=6, column=1).value = row['source']
+        title_toc = row['title'] + ", " + row['subtitle1'] + ", " + row['subtitle2']
+      else:
+        dest_ws.cell(row=5, column=1).value = row['source']
+        title_toc = row['title'] + ", " + row['subtitle1']
+      
+      # Write name and title in the table of contents
+      content = dest_wb["Inhalt"]
+      content.cell(row=count_toc, column=1).value = row['sheet_name']
+      content.cell(row=count_toc, column=2).value = title_toc
+      count_toc = count_toc + 1
+      
+      # saving the destination xlsx file
+      dest_wb.save(dest_file)
+
+
 # Main progam
 def main():
   global data_coll
@@ -205,11 +312,15 @@ def main():
   
   # The sample data will overwrite the data gathered by read_collection
   print("Create sample data")
-  create_sampledata()
+  #create_sampledata()
   
   #read_xls_expl()
   print("Open all excel sheets, read metadata and footnotes and add to dataframes")
   read_all_md_fn()
+  
+  # Loop over the collection and generating the tabsam
+  print("Loop over the collection and generating the tabsam")
+  create_tabsam()
   
   #print(data_coll)
   #print(data_sheet)
