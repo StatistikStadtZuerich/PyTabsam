85397207b09c77e3f0948513910f6b752100edc3
hjstalder <hansi.stalder@gmail.com>
2022-02-28 16:43:15 +0100

    Befüllen der Tabellenblätter und Layout ready for testing.

--- PyTabsam.py
+++ PyTabsam.py
@@ -209,49 +209,48 @@ def read_all_md_fn():
 # Create and generate the destination excel files
 # If the destination files already exists, they will be overwritten
 def create_tabsam():
-    global path_output
-    coll_ID = 0
-    for index, row in data_coll.iterrows():
+  global path_output
+  coll_ID = 0
+  for index, row in data_coll.iterrows():
+    # Copy the template into the existing xlsx files according to the collection
+    coll_ID = row['id']
+    #print(coll_ID)
+    filename = path_output + '/' + row['output_filename']
+    shutil.copy('VorlageTabsam.xlsx', filename)
       
-      # Copy the template into the existing xlsx files according to the collection
-      coll_ID = row['id']
-      #print(coll_ID)
-      filename = path_output + '/' + row['output_filename']
-      shutil.copy('VorlageTabsam.xlsx', filename)
+    # Fill in the creation date into the worksheet 'Inhalt' 
+    file = openpyxl.load_workbook(filename)
+    sheet = file["Inhalt"]
+    today = datetime.date.today()
+    create_date = "Erstellt am: " + today.strftime("%d.%m.%Y")
+    sheet.cell(row=2, column=3).value = create_date
+    file.save(filename)
       
-      # Fill in the creation date into the worksheet 'Inhalt' 
-      file = openpyxl.load_workbook(filename)
-      sheet = file["Inhalt"]
-      today = datetime.date.today()
-      create_date = "Erstellt am: " + today.strftime("%d.%m.%Y")
-      sheet.cell(row=2, column=3).value = create_date
-      file.save(filename)
-      
-      # Create and fill in the worksheet 'Erläuterungen' if exists
-      create_worksheet_expl(coll_ID, filename)
+    # Create and fill in the worksheet 'Erläuterungen' if exists
+    create_worksheet_expl(coll_ID, filename)
 
-      # Create and fill all worksheet according to the dataframe sheet
-      create_worksheets(coll_ID, filename)
+    # Create and fill all worksheet according to the dataframe sheet
+    create_worksheets(coll_ID, filename)
   
 
 # Function create_worksheet_expl
-# Copy the explanation from the source worksheet "Internet"
+# Read the data from the source worksheet "Internet" and write it to the destination worksheet
 # Copy all the format of the source worksheet "Internet"
-# Write the destination worksheet
 # Write the titel in the table of content in the worksheet "Inhalt"
+# Save the xlsx file
 def create_worksheet_expl(coll_ID, dest_file):
   for index, row in data_expl.iterrows():
     if coll_ID == row['FK_collection']:
-      # opening the source xlsx
+      # Opening the source xlsx
       source_xlsx = row['directory'] + "/" + row['filename']
       source_wb = openpyxl.load_workbook(source_xlsx)
       source_ws = source_wb["Internet"]
       
-      # opening the destination xlsx and create the new worksheet
+      # Opening the destination xlsx and create the new worksheet
       dest_wb = openpyxl.load_workbook(dest_file)
       dest_ws = dest_wb.create_sheet("Erläuterungen")
 
-      # Read the data from the source worksheet and write it to the destination worksheet
+      # Read the data from the source worksheet "Internet" and write it to the destination worksheet
       # Copy all the format of the source worksheet "Internet"
       read_write_data(source_ws, dest_ws, 0)
 
@@ -260,14 +259,22 @@ def create_worksheet_expl(coll_ID, dest_file):
       content.cell(row=10, column=1).value = "Erläuterungen"
       content.cell(row=10, column=2).value = ""
   
-      # saving the destination xlsx file
+      # Saving the destination xlsx file
       dest_wb.save(dest_file)
       
 
 # Function create_worksheets
+# Write the header of the sheet
+# Read the data from the source worksheet "Internet" and write it to the destination worksheet
+# Copy all the format of the source worksheet "Internet"
+# Set the uniform row height to 12.75 for the common worksheet
+# Write the titel in the table of content in the worksheet "Inhalt"
+# Save the xlsx file
 def create_worksheets(coll_ID, dest_file):
+  # Set the uniform row height in the destination worksheet
+  row_height = 11.25
   
-  # set the counter for the table of content
+  # Set the counter for the table of content
   wb = openpyxl.load_workbook(dest_file, read_only=True)
   if 'Erläuterungen' in wb.sheetnames:
     count_toc = 11
@@ -277,12 +284,12 @@ def create_worksheets(coll_ID, dest_file):
   for index, row in data_sheet.iterrows():
     if coll_ID == row['FK_collection']:
 
-      # opening the source xlsx
+      # Opening the source xlsx
       source_xlsx = row['directory'] + "/" + row['filename']
       source_wb = openpyxl.load_workbook(source_xlsx)
       source_ws = source_wb["Internet"]
 
-      # opening the destination xlsx and create the new worksheet
+      # Opening the destination xlsx and create the new worksheet
       dest_wb = openpyxl.load_workbook(dest_file)
       dest_ws = dest_wb.create_sheet(row['sheet_name'])
       
@@ -296,7 +303,7 @@ def create_worksheets(coll_ID, dest_file):
         dest_ws.cell(row=3, column=1).font = Font(name='Arial', size=8)
         dest_ws.cell(row=4, column=1).value = row['subtitle2']
         dest_ws.cell(row=4, column=1).font = Font(name='Arial', size=8)
-        dest_ws.cell(row=6, column=1).value = row['source']
+        dest_ws.cell(row=6, column=1).value = "Quelle: " + row['source']
         dest_ws.cell(row=6, column=1).font = Font(name='Arial', size=8)
         title_toc = row['title'] + ", " + row['subtitle1'] + ", " + row['subtitle2']
         # define the row, where the content starts
@@ -304,7 +311,7 @@ def create_worksheets(coll_ID, dest_file):
       if(row['subtitle1'] != "None" and row['subtitle2'] == "None"):
         dest_ws.cell(row=3, column=1).value = row['subtitle1']
         dest_ws.cell(row=3, column=1).font = Font(name='Arial', size=8)
-        dest_ws.cell(row=5, column=1).value = row['source']
+        dest_ws.cell(row=5, column=1).value = "Quelle: " + row['source']
         dest_ws.cell(row=5, column=1).font = Font(name='Arial', size=8)
         title_toc = row['title'] + ", " + row['subtitle1']
         # define the row, where the content starts
@@ -312,7 +319,7 @@ def create_worksheets(coll_ID, dest_file):
       if(row['subtitle1'] == "None" and row['subtitle2'] != "None"):
         dest_ws.cell(row=3, column=1).value = row['subtitle2']
         dest_ws.cell(row=3, column=1).font = Font(name='Arial', size=8)
-        dest_ws.cell(row=5, column=1).value = row['source']
+        dest_ws.cell(row=5, column=1).value = "Quelle: " + row['source']
         dest_ws.cell(row=5, column=1).font = Font(name='Arial', size=8)
         title_toc = row['title'] + ", " + row['subtitle2']
         # define the row, where the content starts
@@ -322,6 +329,11 @@ def create_worksheets(coll_ID, dest_file):
       # Copy all the format of the source worksheet "Internet"
       read_write_data(source_ws, dest_ws, row_start)
       
+      # Set the uniform row height to 12.75 for the common worksheet
+      maxrow = dest_ws.max_row
+      for r in range (1, maxrow + 100):
+        dest_ws.row_dimensions[r].height = row_height
+
       # Write name and title in the table of contents
       content = dest_wb["Inhalt"]
       content.cell(row=count_toc, column=1).value = row['sheet_name']
@@ -333,49 +345,40 @@ def create_worksheets(coll_ID, dest_file):
 
 # Function read_write_data
 def read_write_data(source_ws, dest_ws, row_start):
+  # set specific column width and hidden property
+  # we cannot copy the entire column_dimensions attribute so we copy selected attributes
+  for key, value in source_ws.column_dimensions.items():
+    # Excel actually groups multiple columns under 1 key. Use the min max attribute to also group the columns in the targetSheet
+    dest_ws.column_dimensions[key].min = copy(source_ws.column_dimensions[key].min)   
+    dest_ws.column_dimensions[key].max = copy(source_ws.column_dimensions[key].max)
+    # set width for every column
+    dest_ws.column_dimensions[key].width = copy(source_ws.column_dimensions[key].width) 
+    dest_ws.column_dimensions[key].hidden = copy(source_ws.column_dimensions[key].hidden)
 
-      #dest_ws.sheet_format = copy(source_ws.sheet_format)
-      #dest_ws.sheet_properties = copy(source_ws.sheet_properties)
-      #dest_ws.merged_cells = copy(source_ws.merged_cells)
-      #dest_ws.page_margins = copy(source_ws.page_margins)
-      #dest_ws.freeze_panes = copy(source_ws.freeze_panes)
-
-      #for key1, value in source_ws.row_dimensions.items():
-        # funktioniert nicht
-        #dest_ws.row_dimensions[key1].height = copy(source_ws.row_dimensions[key1].height)
-
-      # set specific column width and hidden property
-      # we cannot copy the entire column_dimensions attribute so we copy selected attributes
-      for key, value in source_ws.column_dimensions.items():
-        # Excel actually groups multiple columns under 1 key. Use the min max attribute to also group the columns in the targetSheet
-        dest_ws.column_dimensions[key].min = copy(source_ws.column_dimensions[key].min)   
-        dest_ws.column_dimensions[key].max = copy(source_ws.column_dimensions[key].max)
-        # set width for every column
-        dest_ws.column_dimensions[key].width = copy(source_ws.column_dimensions[key].width) 
-        dest_ws.column_dimensions[key].hidden = copy(source_ws.column_dimensions[key].hidden)
-
-      # calculate total number of rows and 
-      # columns in source excel file
-      mr = source_ws.max_row
-      mc = source_ws.max_column
+  # calculate total number of rows and 
+  # columns in source excel file
+  mr = source_ws.max_row
+  mc = source_ws.max_column
       
-      # copying the cell values from source 
-      # excel file to destination xlsx file
-      for i in range (1, mr + 1):
-        for j in range (1, mc + 1):
-          # reading cell value from source xlsx
-          c = source_ws.cell(row = i, column = j)
-  
-          # writing the read value to destination xlsx file
-          dest_ws.cell(row = i+row_start, column = j).value = c.value
+  # copying the cell values from source 
+  # excel file to destination xlsx file
+  for i in range (1, mr + 1):
+    for j in range (1, mc + 1):
+      # reading cell value from source xlsx
+      c = source_ws.cell(row = i, column = j)
+
+      # writing the read value to destination xlsx file
+      dest_ws.cell(row = i+row_start, column = j).value = c.value
           
-          # set the color to black and copy font
-          c.font = c.font.copy(color = 'FF000000')
-          dest_ws.cell(row = i+row_start, column = j).font = copy(c.font)
+      # set the color to black and copy font
+      c.font = c.font.copy(color = 'FF000000')
+      dest_ws.cell(row = i+row_start, column = j).font = copy(c.font)
           
-          # copy alignment
-          dest_ws.cell(row = i+row_start, column = j).alignment = copy(c.alignment)
+      # copy alignment
+      dest_ws.cell(row = i+row_start, column = j).alignment = copy(c.alignment)
       
+      # copy number_format
+      dest_ws.cell(row = i+row_start, column = j).number_format = copy(c.number_format)
 
 # Main progam
 def main():
